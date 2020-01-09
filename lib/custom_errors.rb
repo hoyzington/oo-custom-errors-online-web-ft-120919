148bfcfd7fe869d6bc4a28c09ec48daf615949a9
Scott Hoisington <hoyzington@gmail.com>
2020-01-09 00:00:21 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,9 +10,9 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      
-      raise PartnerError
-      
+      begin
+        raise PartnerError
+      rescue PartnerError => error
     else
       person.partner = self
     end
